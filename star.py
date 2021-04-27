c68ee05cb360f0ca1b9b617837bc38de215f327f
Atharv Puranik <68159253+Lucifer-1010@users.noreply.github.com>
2021-04-27 18:58:26 +0530

    Add files via upload

---
+++ star.py
@@ -0,0 +1,18 @@
+import turtle
+sc=turtle.Screen()
+sc.setup(600,800)
+spiral=turtle.Turtle()
+spiral.speed(5)
+sc.bgcolor("black")
+sc.title("Lucifer")
+col=["yellow","blue","White","green"]
+c=0
+
+for i in range(50):
+	spiral.forward(i*10)
+	spiral.right(144)
+	spiral.color(col[c])
+	if c==3:
+		c=0
+	else:
+		c+=1
